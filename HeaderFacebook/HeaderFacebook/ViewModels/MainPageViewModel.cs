5f482afe6a38e3d7b713c1b18e3fec9f5d44d051
guilherme rodrigues souza <guilherme-rsouza@hotmail.com>
2017-10-06 00:36:37 -0300

    Add project files.

---
+++ HeaderFacebook/HeaderFacebook/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace HeaderFacebook.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        public ObservableCollection<string> Itens { get; set; }
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainPageViewModel()
+        {
+            Itens = new ObservableCollection<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                Itens.Add(i.ToString());
+            }
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
---
+++ HeaderFacebook/HeaderFacebook/Views/Controls/ListViewFacebook.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace HeaderFacebook.Views.Controls
+{
+    public class ListViewFacebook : ScrollView
+    {
+        public ListViewFacebook()
+        {
+            Scrolled += (sender, e) => Parallax();
+        }
+        public static readonly BindableProperty ParallaxViewProperty =
+            BindableProperty.Create(nameof(ListViewFacebook), typeof(View), typeof(ListViewFacebook), null);
+
+        public View ParallaxView
+        {
+            get { return (View)GetValue(ParallaxViewProperty); }
+            set { SetValue(ParallaxViewProperty, value); }
+        }
+
+        public static readonly BindableProperty HeaderViewProperty =
+            BindableProperty.Create(nameof(ListViewFacebook), typeof(View), typeof(ListViewFacebook), null);
+
+        public View HeaderView
+        {
+            get { return (View)GetValue(HeaderViewProperty); }
+            set { SetValue(HeaderViewProperty, value); }
+        }
+
+        double height, maxPositionHeader;
+        public void Parallax()
+        {
+            if (ParallaxView == null)
+                return;
+
+            if (height <= 0)
+                height = ParallaxView.Height;
+
+            var y = -(int)((float)ScrollY / 2.5f);
+            if (y < 0)
+            {
+                //Move a imagem no eixo Y em uma fração da posição Y do ScrollView.
+                ParallaxView.Scale = 1;
+                ParallaxView.TranslationY = y;
+                var teste = HeaderView.Y;
+                if (HeaderView.TranslationY <= 0)
+                    HeaderView.TranslationY = -ScrollY /1.5;
+            }
+            else if (Device.OS == TargetPlatform.iOS)
+            {
+                //Calcula uma escala que iguala a altura x scroll.
+                double newHeight = height + (ScrollY * -1);
+                ParallaxView.Scale = newHeight / height;
+                ParallaxView.TranslationY = -(ScrollY / 2);
+            }
+            else
+            {
+                ParallaxView.Scale = 1;
+                ParallaxView.TranslationY = 0;
+            }
+        }
+    }
+}
